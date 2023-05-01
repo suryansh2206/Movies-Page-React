21cdd3682b8ebcf2032ba850bbeaebf041eda16d
suryansh2206 <suryansh804.sg@gmail.com>
2023-05-01 22:31:14 +0530

    delete button added

--- src/App.js
+++ src/App.js
@@ -12,21 +12,33 @@ const App = () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("https://react-http-a080a-default-rtdb.firebaseio.com/movies.json");
       if (!response.ok) {
         throw new Error("Something went wrong");
       }
 
       const data = await response.json();
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_data,
-        };
-      });
-      setMovies(transformedMovies);
+
+      const loadedMovies = []
+
+      for(const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_data,
+      //   };
+      // });
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -37,8 +49,16 @@ const App = () => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  const addMovieHandler = (movie) => {
-    console.log(movie)
+  const addMovieHandler = async (movie) => {
+    const response = await fetch("https://react-http-a080a-default-rtdb.firebaseio.com/movies.json", {
+      method: 'POST', 
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    console.log(data)
   }
 
   return (
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import Movie from './Movie';
-import classes from './MoviesList.module.css';
+import Movie from "./Movie";
+import classes from "./MoviesList.module.css";
 
 const MovieList = (props) => {
+  const deleteMovieHandler = async (key) => {
+    try {
+      const response = await fetch(
+        "https://react-http-a080a-default-rtdb.firebaseio.com/movies.json",
+        {
+          method: "DELETE",
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <ul className={classes['movies-list']}>
+    <ul className={classes["movies-list"]}>
       {props.movies.map((movie) => (
-        <Movie
-          key={movie.id}
-          title={movie.title}
-          releaseDate={movie.releaseDate}
-          openingText={movie.openingText}
-        />
+        <>
+          <Movie
+            key={movie.id}
+            title={movie.title}
+            releaseDate={movie.releaseDate}
+            openingText={movie.openingText}
+          />
+          <button onClick={() => deleteMovieHandler(movie.id)}>
+            Delete Movie
+          </button>
+        </>
       ))}
     </ul>
   );
